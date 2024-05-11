40b29cb737b894a37200dad297c0431e18bfda99
Hongkai Ma <mahongkaichn@gmail.com>
2024-05-11 17:57:52 +0800

    Update Post.js

--- blog.config.js
+++ blog.config.js
@@ -3,7 +3,7 @@ const BLOG = {
   author: 'KeyAI',
   email: 'mail@keyai.me',
   link: 'https://blog.keyai.me',
-  description: '日常 & 技术 & 折腾',
+  description: '',
   lang: 'zh-CN', // ['en-US', 'zh-CN', 'zh-HK', 'zh-TW', 'ja-JP', 'es-ES']
   timezone: 'Asia/Shanghai', // Your Notion posts' date will be interpreted as this timezone. See https://en.wikipedia.org/wiki/List_of_tz_database_time_zones for all options.
   appearance: 'auto', // ['light', 'dark', 'auto'],
